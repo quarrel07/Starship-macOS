HarbourMasters/Starship

Fixed more wrong textures

src/engine/fox_versus.c
@@ -163,10 +163,7 @@ void func_versus_800BD350(f32 xPos, f32 yPos) {
 }
 
 void func_versus_800BD3A8(f32 xPos, f32 yPos) {
-    s32 i = 0;
-
-    TextureRect_CI4(&gMasterDisp, D_VS_MENU_70118E0 + ((80 * 8 * i) / 2), D_VS_MENU_70123F8, 80, 71, xPos, yPos, 1.0f,
-                     1.0f);
+    TextureRect_CI4(&gMasterDisp, D_VS_MENU_70118E0, D_VS_MENU_70123F8, 80, 71, xPos, yPos, 1.0f, 1.0f);
 }
 
 void func_versus_800BD4D4(f32 xPos, f32 yPos, s32 arg2) {
@@ -243,12 +240,10 @@ void func_versus_800BDAA0(f32 xPos, f32 yPos, s32 arg2, s32 arg3, s32 arg4) {
             TextureRect_IA8(&gMasterDisp, D_800D4B48[temp1], 16, 15, xPos, yPos, 0.7f, 0.7f);
         } else {
             if (temp3 > y3) {
-                TextureRect_IA8(&gMasterDisp, D_800D4B48[temp2] + (16 * arg4), 16, 15 - arg4, xPos, yPos, 0.7f, 0.7f);
-                TextureRect_IA8(&gMasterDisp, D_800D4B48[temp1], 16, arg4, xPos, yPos + (15 - arg4) * 0.7f, 0.7f, 0.7f);
+                TextureRect_IA8(&gMasterDisp, D_800D4B48[temp2], 16, arg4, xPos, yPos, 0.7f, 0.7f);
             }
             if (temp3 < y3) {
-                TextureRect_IA8(&gMasterDisp, D_800D4B48[temp2], 16, 15 - arg4, xPos, yPos + (arg4 * 0.7f), 0.7f, 0.7f);
-                TextureRect_IA8(&gMasterDisp, D_800D4B48[temp1] + (16 * (15 - arg4)), 16, arg4, xPos, yPos, 0.7f, 0.7f);
+                TextureRect_IA8(&gMasterDisp, D_800D4B48[temp2], 16, arg4, xPos, yPos, 0.7f, 0.7f);
             }
         }
         xPos += 13.0f * 0.7f;

quarrel07/Starship-macOS

src/engine/fox_versus.c
@@ -88,11 +88,7 @@ void func_versus_800BC88C(f32 xPos, f32 yPos, f32 scale) {
 }
 
 void func_versus_800BC8D8(f32 xPos, f32 yPos, f32 scale) {
-<<<<<<< HEAD
     Lib_TextureRect_CI8(&gMasterDisp, D_versus_300A470, D_versus_300B218, 152, 23, xPos, yPos, scale, scale);
-=======
-    TextureRect_CI8(&gMasterDisp, D_versus_300A470, D_versus_300B218, 152, 23, xPos, yPos, scale, scale);
->>>>>>> edd7dba2 (Fixed all remaining compilation issues)
 }
 
 void func_versus_800BC9DC(f32 xPos, f32 yPos, f32 scale, s32 yScale) {
@@ -100,20 +96,12 @@ void func_versus_800BC9DC(f32 xPos, f32 yPos, f32 scale, s32 yScale) {
     u8* D_800D4ABC[] = { D_versus_30087A0, D_versus_3008EC0, D_versus_3009990 };
     s32 D_800D4AB0[] = { 40, 64, 64 };
 
-<<<<<<< HEAD
     Lib_TextureRect_CI8(&gMasterDisp, D_800D4ABC[yScale], D_800D4AA4[yScale], D_800D4AB0[yScale], 40, xPos, yPos, scale,
-=======
-    TextureRect_CI8(&gMasterDisp, D_800D4ABC[yScale], D_800D4AA4[yScale], D_800D4AB0[yScale], 40, xPos, yPos, scale,
->>>>>>> edd7dba2 (Fixed all remaining compilation issues)
                      scale);
 }
 
 void func_versus_800BCB44(f32 xPos, f32 yPos, f32 scale) {
-<<<<<<< HEAD
     Lib_TextureRect_CI8(&gMasterDisp, D_versus_3006C60, D_versus_3007500, 96, 23, xPos, yPos, scale, scale);
-=======
-    TextureRect_CI8(&gMasterDisp, D_versus_3006C60, D_versus_3007500, 96, 23, xPos, yPos, scale, scale);
->>>>>>> edd7dba2 (Fixed all remaining compilation issues)
 }
 
 void func_versus_800BCC48(f32 xPos, f32 yPos, f32 xScale, f32 yScale, s32 arg4) {
@@ -121,16 +109,11 @@ void func_versus_800BCC48(f32 xPos, f32 yPos, f32 xScale, f32 yScale, s32 arg4) 
     u8* D_800D4AD8[] = { D_versus_3006040, D_versus_30076C0, D_versus_300B3F0, D_versus_3004F60 };
     s32 D_800D4AE8[] = { 104, 152, 168, 152 };
 
-<<<<<<< HEAD
     Lib_TextureRect_CI8(&gMasterDisp, D_800D4AD8[arg4], D_800D4AC8[arg4], D_800D4AE8[arg4], 25, xPos, yPos, xScale,
-=======
-    TextureRect_CI8(&gMasterDisp, D_800D4AD8[arg4], D_800D4AC8[arg4], D_800D4AE8[arg4], 25, xPos, yPos, xScale,
->>>>>>> edd7dba2 (Fixed all remaining compilation issues)
                      yScale);
 }
 
 void func_versus_800BCE24(f32 xPos, f32 yPos, f32 xScale, f32 yScale) {
-<<<<<<< HEAD
     Lib_TextureRect_CI8(&gMasterDisp, D_versus_3001420, D_versus_3003E20, 256, 42, xPos, yPos, xScale, yScale);
 }
 
@@ -138,13 +121,6 @@ void func_versus_800BCEF8(f32 xPos, f32 yPos, f32 scale) {
     s32 i;
 
     Lib_TextureRect_CI8(&gMasterDisp, D_versus_3004010, D_versus_3004D58, 136, 25, xPos, yPos, scale, scale);
-=======
-    TextureRect_CI8(&gMasterDisp, D_versus_3001420, D_versus_3003E20, 256, 42, xPos, yPos, xScale, yScale);
-}
-
-void func_versus_800BCEF8(f32 xPos, f32 yPos, f32 scale) {
-    TextureRect_CI8(&gMasterDisp, D_versus_3004010, D_versus_3004D58, 136, 25, xPos, yPos, scale, scale);
->>>>>>> edd7dba2 (Fixed all remaining compilation issues)
 }
 
 void func_versus_800BCFFC(f32 xPos, f32 yPos, f32 xScale, f32 yScale) {
@@ -192,15 +168,9 @@ void func_versus_800BD350(f32 xPos, f32 yPos) {
 }
 
 void func_versus_800BD3A8(f32 xPos, f32 yPos) {
-<<<<<<< HEAD
     s32 i;
 
     Lib_TextureRect_CI4(&gMasterDisp, aVsHandicapFrameTex + ((80 * 8 * i) / 2), aVsHandicapFrameTLUT, 80, 71, xPos, yPos, 1.0f,
-=======
-    s32 i = 0;
-
-    TextureRect_CI4(&gMasterDisp, D_VS_MENU_70118E0 + ((80 * 8 * i) / 2), D_VS_MENU_70123F8, 80, 71, xPos, yPos, 1.0f,
->>>>>>> edd7dba2 (Fixed all remaining compilation issues)
                      1.0f);
 }
 
@@ -277,17 +247,8 @@ void func_versus_800BDAA0(f32 xPos, f32 yPos, s32 arg2, s32 arg3, s32 arg4) {
         if (temp1 == temp2) {
             Lib_TextureRect_IA8(&gMasterDisp, D_800D4B48[temp1], 16, 15, xPos, yPos, 0.7f, 0.7f);
         } else {
-            if (temp3 > y3) {
-                Lib_TextureRect_IA8(&gMasterDisp, D_800D4B48[temp2] + (16 * arg4), 16, 15 - arg4, xPos, yPos, 0.7f,
-                                    0.7f);
-                Lib_TextureRect_IA8(&gMasterDisp, D_800D4B48[temp1], 16, arg4, xPos, yPos + (15 - arg4) * 0.7f, 0.7f,
-                                    0.7f);
-            }
             if (temp3 < y3) {
-                Lib_TextureRect_IA8(&gMasterDisp, D_800D4B48[temp2], 16, 15 - arg4, xPos, yPos + (arg4 * 0.7f), 0.7f,
-                                    0.7f);
-                Lib_TextureRect_IA8(&gMasterDisp, D_800D4B48[temp1] + (16 * (15 - arg4)), 16, arg4, xPos, yPos, 0.7f,
-                                    0.7f);
+                Lib_TextureRect_IA8(&gMasterDisp, D_800D4B48[temp2], 16, arg4, xPos, yPos, 0.7f, 0.7f);
             }
         }
         xPos += 13.0f * 0.7f;