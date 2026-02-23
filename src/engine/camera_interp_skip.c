ac05adb5de21a8ad59eb71397606348bbda86a10
Sonic Dreamcaster <alejandro.asenjo88@gmail.com>
2026-02-23 03:53:41 -0300

    camera_interp_skip.c: include guards

--- src/engine/camera_interp_skip.c
+++ src/engine/camera_interp_skip.c
@@ -1,3 +1,6 @@
+#ifndef CAMERA_INTERP_SKIP_C
+#define CAMERA_INTERP_SKIP_C
+
 #include "global.h"
 
 #define SQXYZ(vec) ((vec.x) * (vec.x) + (vec.y) * (vec.y) + (vec.z) * (vec.z))
@@ -106,3 +109,5 @@ bool should_interpolate_perspective(Vec3f* eye, Vec3f* at) {
 
     return true;
 }
+
+#endif
